8634504c6f18f2421c66669c554d15a7d7a775c4
KangggX <lowkangxuan@gmail.com>
2022-04-09 18:11:45 +0800

    testing config

--- .eleventy.js
+++ .eleventy.js
@@ -50,8 +50,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
     eleventyConfig.addWatchTarget("./src/css/");
 
-    eleventyConfig.addPassthroughCopy("./src/images/");
-    eleventyConfig.addWatchTarget("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/projects/");
+    eleventyConfig.addWatchTarget("./src/projects/");
     return {
       htmlTemplateEngine: "njk",
       markdownTemplateEngine: "njk",
